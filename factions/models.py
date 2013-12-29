8bc39cdba0bae5cd1b96c75d0a3ed956b7e20b9b
Kyle O'Donnell <kah.odonnell@gmail.com>
2013-12-29 06:37:37 +0000

    heroku ready- attempt 1

--- factions/models.py
+++ factions/models.py
@@ -4,5 +4,3 @@ class Faction(models.Model):
     name = models.CharField(max_length=100)
     motto = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
-
-# Create your models here.
